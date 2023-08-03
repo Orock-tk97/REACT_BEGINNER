2250835a561d48cc6a6c3a5a1ad8f1c6e6a52b95
Orocktk <kingsagbor97@gmail.com>
2023-08-03 11:04:07 +0100

    Final Project

---
+++ dojo-blog/src/BlogDetails.js
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const{data: blog, error, isPending}= useFetch(' http://localhost:6149/blogs/'+ id);
+  const history = useHistory(); 
+  const handleClick = () =>{
+    fetch('http://localhost:6149/blogs' + blog.id, {
+      method: 'DELETE'
+    }).then(()=>{
+      history.push('/');
+
+    })
+  }
+
+  return (
+    <div className="blog-details">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      <h2>Blog details - { id }</h2>
+      {blog &&(
+        <article>
+          <h2>{blog.title}</h2>
+          <p>written by {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
+        </article>
+      )}
+    </div>
+  );
+}
+ 
+export default BlogDetails;
